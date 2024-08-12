cb35609ef4955f9241608c930ba8414f70df35c2
xzb <2598514867@qq.com>
2024-08-12 22:03:05 +0800

    post_hook

--- lua/live-rename.lua
+++ lua/live-rename.lua
@@ -297,6 +297,42 @@ function M.update()
 	vim.api.nvim_win_set_width(C.win, text_width + 2)
 end
 
+local post_hook = function(result)
+	if not result or not result.changes then
+		print(string.format("could not perform rename"))
+		return
+	end
+
+	local notification, entries = "", {}
+	local num_files, num_updates = 0, 0
+	for uri, edits in pairs(result.changes) do
+		num_files = num_files + 1
+		local bufnr = vim.uri_to_bufnr(uri)
+
+		for _, edit in ipairs(edits) do
+			local start_line = edit.range.start.line + 1
+			local line = vim.api.nvim_buf_get_lines(bufnr, start_line - 1, start_line, false)[1]
+
+			num_updates = num_updates + 1
+			table.insert(entries, {
+				bufnr = bufnr,
+				lnum = start_line,
+				col = edit.range.start.character + 1,
+				text = line,
+			})
+		end
+
+		local short_uri = string.sub(vim.uri_to_fname(uri), #vim.fn.getcwd() + 2)
+		notification = notification .. string.format("made %d change(s) in %s\n", #edits, short_uri)
+	end
+
+	if num_files > 1 then
+		print(notification)
+		vim.fn.setqflist(entries, "r")
+		FeedKeys("<c-q>", "m")
+	end
+end
+
 function M.submit()
 	local new_text = vim.api.nvim_buf_get_lines(C.buf, 0, 1, false)[1]
 	local mode = vim.api.nvim_get_mode().mode
@@ -315,6 +351,7 @@ function M.submit()
 		local handler = C.client.handlers[lsp_methods.textDocument_rename]
 			or vim.lsp.handlers[lsp_methods.textDocument_rename]
 		handler(resp.err, resp.result, resp.context, resp.config)
+		post_hook(resp.result)
 	end
 
 	M.hide()
